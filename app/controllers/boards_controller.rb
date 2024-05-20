6c7c283c93f1b70310121affa7d8dd44fef6b27a
syuuuuuuuuuuuuuuuu <164719063+syuuuuuuuuuuuuuuuu@users.noreply.github.com>
2024-05-20 11:12:44 +0900

    user_idのひも図家

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,9 @@
+class BoardsController < ApplicationController
+  def index
+    @boards = Board.all
+  end
+
+  def new
+    @board = Board.new()
+  end
+end
--- db/migrate/20240519112001_create_boards.rb
+++ db/migrate/20240519112001_create_boards.rb
@@ -1,7 +1,6 @@
 class CreateBoards < ActiveRecord::Migration[6.1]
   def change
     create_table :boards do |t|
-      t.references :user, null: false
       t.string :title, null: false
       t.string :content, null: false
 
