cfa66f0c951403e786ecb77cb1b44d98a69bcd8a
syuuuuuuuuuuuuuuuu <164719063+syuuuuuuuuuuuuuuuu@users.noreply.github.com>
2024-05-20 14:33:51 +0900

    board index,new の実装

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,8 +2,25 @@ class BoardsController < ApplicationController
   def index
     @boards = Board.all
   end
+  def show
+    @board = Board.find(params[:id])
+  end
 
   def new
-    @board = Board.new()
+    @board = current_user.boards.build
   end
+
+  def create
+    @board = current_user.boards.build(board_params)
+    if @board.save
+      redirect_to boards_path, notice: 'Board is create'
+    else
+      flash.now[:error] = '保存に失敗'
+      render :new
+    end
+  end
+  private
+    def board_params
+      params.require(:board).permit(:title, :content)
+    end
 end
