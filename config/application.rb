571ad4ebd9d13c4f5375f3df33b57dfbbc0adff1
syuuuuuuuuuuuuuuuu <164719063+syuuuuuuuuuuuuuuuu@users.noreply.github.com>
2024-05-26 22:21:24 +0900

    babelの変更

--- config/application.rb
+++ config/application.rb
@@ -10,11 +10,13 @@ module TodoApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+
     if Rails.env.development? || Rails.env.test?
-      Bundler.require(*Rails.groups)
       Dotenv::Railtie.load
     end
+
     config.i18n.default_locale = :ja
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
